baf57dabd8e85f94d8ffe18353c0c1392780a622
Michel Santos Stefani <michell.stefani@msn.com>
2025-02-05 16:54:41 -0300

    refactor: fix site

--- assets/ThreeScene-BIqF-Slf.js
+++ assets/ThreeScene-BIqF-Slf.js
@@ -1,4 +1,4 @@
-import{r as zs,j as xo}from"./index-Ci3fHjio.js";/**
+import{r as zs,j as xo}from"./index-CKUj1rnG.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
