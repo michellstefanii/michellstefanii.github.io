ad86f7b3eb014daa1b82f77dbd5339d91b4cc7fd
Michel Santos Stefani <michell.stefani@msn.com>
2025-02-11 13:42:25 -0300

    refactor: add project

--- assets/ThreeScene-DngdhCtt.js
+++ assets/ThreeScene-DngdhCtt.js
@@ -1,4 +1,4 @@
-import{r as zs,j as xo}from"./index-CKUj1rnG.js";/**
+import{r as zs,j as xo}from"./index-BPgdgD6V.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
